e9e7198e42cde7d45dca306f03d6d689a748f7db
nokrattana <ratanawadee94@hotmail.com>
2021-09-14 12:38:20 +0100

    Add Ticker function in app Menu

--- mystocks/market/views.py
+++ mystocks/market/views.py
@@ -10,16 +10,19 @@ def about_temp(request):
     import requests
     import json
 
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("")
 
-    api_request = requests.get("")
-
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
 
 
 
-    return render(request, 'blog_temp.html', {'api': api})
+        return render(request, 'blog_temp.html', {'api': api})
+    else:
+        return render(request, 'blog_temp.html', {'ticker': "Enter symbol.."})
 
 # Api key here.
